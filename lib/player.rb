610656b8ac6d65970a321d4bb9c4f73d8523b277
ReignBoo <reignboo3281@gmail.com>
2022-08-31 18:29:14 +0800

    Update player class to collect instance markers

--- lib/player.rb
+++ lib/player.rb
@@ -1,6 +1,11 @@
 class Player
+    @@markers = []
+
+    attr_reader :name, :marker
+
     def initialize(name, marker)
         @name = name
         @marker = marker
+        @@markers << marker
     end
 end
