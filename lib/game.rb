ecf8b462a695de24709ea3cd8d1f2425cd664f48
ReignBoo <reignboo3281@gmail.com>
2022-09-01 14:51:06 +0800

    Add player win check to game class

--- lib/game.rb
+++ lib/game.rb
@@ -7,23 +7,38 @@ class Game
     private
     
     def start
+        @board = Board.new
+
         puts "Player 1, please input your name: "
         name1 = gets.chomp
         puts "#{name1}, please input your marker (should only be 1 character long): "
         marker1 = gets.chomp until marker1.to_s.length == 1
-        @player1 = Player.new(name1, marker1)
+        @player1 = Player.new(name1, marker1, @board)
 
         puts "Player 2, please input your name: "
         name2 = gets.chomp
         puts "#{name2}, please input your marker (should only be 1 character long and is not #{marker1}): "
         marker2 = gets.chomp until marker2.to_s.length == 2 && marker2.to_s != marker1.to_s
-        @player2 = Player.new(name2, marker2)
-
-        @board = Board.new
+        @player2 = Player.new(name2, marker2, @board)
     end
 
     def end?
         (@board.board - Player.markers) == Player.markers
+    end
+
+    def player_win?(player)
+        WINNING_COMBO = [
+            [1,2,3], [4,5,6], [7,8,9], [1,4,7], [2,5,8], [3,6,9], [1,5,9], [3,5,7]
+        ]
+        
+        is_win = false
+
+        WINNING_COMBO.each do |win_check|
+            is_win = (player.marker_positions - win_check) == win_check
+            break if is_win
+        end
+
+        is_win
     end
 
     def game_loop
