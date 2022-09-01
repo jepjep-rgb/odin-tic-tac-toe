7166891401fe02271ecf7fab3af6457120ef028a
ReignBoo <reignboo3281@gmail.com>
2022-09-01 14:50:37 +0800

    Add place marker on board method to player class

--- lib/player.rb
+++ lib/player.rb
@@ -1,11 +1,19 @@
 class Player
     @@markers = []
 
-    attr_reader :name, :marker
+    attr_reader :name, :marker, :marker_positions
 
-    def initialize(name, marker)
+    def initialize(name, marker, board)
         @name = name
         @marker = marker
+        @board = board
+        @marker_positions = []
         @@markers << marker
     end
+
+    def place_marker(position)
+        @marker_positions << position
+        @board.update_board(position, @marker)
+    end
+
 end
